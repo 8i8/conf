41334cb16f52251afa6f57f2a0a8c8380b7dca72
8i8 <iain.coding@gmail.com>
2021-06-07 13:52:15 +0200

    documentation updated

--- doc.go
+++ doc.go
@@ -1,18 +1,20 @@
 /*
-Package conf helps you to organise and maintain package sub-commands,
-their options and their flags.
+Package conf helps you to organise and maintain comandline application,
+facilitating flags and their documentation including sub-commands.
 
-COMMANDS sub-commands can be created by using the conf.Command function
-which returns a token set to designate the command as a target when
-creating an option.
+COMMANDS sub-commands can be created by using the Config.Command
+function which returns a token that is used to apply options to that
+command set.
 
-	cmd = conf.Command("doit", doitUsageString)
+	c := conf.Config
+	cmd = c.Command("myApplicationName", helpDocumentation)
 
-The cmd token is then used when defining an option, instructing the
-package that the option is to be assigned to the command. The option will
-appear in all of the commands for which tokens are provided, the tokes are
-separated by the | character, indicating that all the delineated tokens
-are to be used.
+The cmd token is used when defining an option, instructing the conf
+package that the option is to be assigned to that command. An option
+will appear within all of the commands for which tokens are provided.
+Tokes are separated combined using the '|' pipe character, indicating
+that the option and its flag is to be applied to all of the named
+command sets.
 
 	conf.Option{
 		Commands: cmd | cmd1 | cmd2
@@ -38,17 +40,18 @@ import (
 )
 
 var (
-	def = conf.Setup(helpBase, helpDef)
-	one = conf.Command("one", helpOne)
-	two = conf.Command("two", helpTwo)
+	c   = &conf.Config{}
+	def = c.Command(helpBase, helpDef)
+	one = c.Command("one", helpOne)
+	two = c.Command("two", helpTwo)
 )
 
 var opts = []conf.Option{
-	{Name: "intie",
+	{
 		Type:     conf.Int,
 		Flag:     "n",
 		Default:  12,
-		Usage:    intie,
+		Usage:    intUse,
 		Commands: def | one | two,
 		Check: func(v interface{}) (interface{}, error) {
 			i := *v.(*int)
@@ -58,11 +61,11 @@ var opts = []conf.Option{
 			return v, nil
 		},
 	},
-	{Name: "thing",
+	{
 		Type:     conf.String,
 		Flag:     "s",
-		Default:  "Some thing",
-		Usage:    thing,
+		Default:  "Some thing in the way she smiles",
+		Usage:    stringUse,
 		Commands: def | one | two,
 		Check: func(v interface{}) (interface{}, error) {
 			s := *v.(*string)
@@ -72,28 +75,28 @@ var opts = []conf.Option{
 			return v, nil
 		},
 	},
-	{Name: "none",
+	{
 		Type:     conf.Int,
 		Flag:     "i",
 		Default:  16,
 		Usage:    "the i is the none of all the ints",
 		Commands: def | one | two,
 	},
-	{Name: "verbosity",
+	{
 		Type:     conf.Int,
 		Flag:     "v",
 		Default:  0,
-		Usage:    "The overall chattiness of it all",
+		Usage:    "Oh! The overall chattiness of it all",
 		Commands: def | one | two,
 	},
 }
 
 func main() {
-	conf.Options(opts...)
-	if err := conf.Parse(); err != nil {
+	err := c.Compose(opts...)
+	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("The current running mode is %q\n", conf.GetCmd())
+	fmt.Printf("The current running mode is %v\n", c.Running())
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -117,13 +120,13 @@ var helpDef = `
 MODES
         conf [mode] -[flag]
 
-	one     one does all things in the oneiest way.
+	one     one does all things that one should do.
 
-	two     two, despite appearances is second to none, doing things in an
-	        agreeable two like fashion.
+	two     two, despite appearances is second to none, achieving
+		things in no less than a second.
 
-	Further details of the use of each mode can be found by running the
-	following command.
+	Further details of the use of each mode can be found by running
+	the following command.
 
 	conf [mode] -help  or conf [mode] -h
 
@@ -131,11 +134,13 @@ FLAGS`
 
 var helpOne = `MODE
 	conf one
+		Best used like this ...
 
 FLAGS`
 
 var helpTwo = `MODE
 	conf two
+		Advisable to use like this ...
 
 FLAGS`
 
@@ -143,11 +148,12 @@ FLAGS`
 //  Flags
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-var intie = `This is the very default value in the most simple mode, to test
-if another way of writing the messages might be better.`
+var intUse = `This is the very default value in the most simplest of modes, to
+	test if another way of writing the messages might could possibly
+	be better.`
 
-var thing = `This is the default string thing, so as to best exemplify
-the use of this package in its current state I thought it
-best to write something very wordy here.`
+var stringUse = ` This is the default string dooing its thing, as to best
+	exemplify the use of this package in its current state; I
+	thought it best to write something particularly wordy here.`
 */
 package conf
