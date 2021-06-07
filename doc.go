fea3a2477bcebecbfdb574740eff41367224d7be
8i8 <iain.coding@gmail.com>
2021-06-07 13:52:15 +0200

    documentation updated

--- doc.go
+++ doc.go
@@ -1,31 +1,63 @@
 /*
-Package conf helps you to organise and maintain package sub-commands,
-their options and their flags.
+Package conf helps you to organise and maintain a packages flags, their
+options and their documention.
 
 COMMANDS sub-commands can be created by using the conf.Command function
 which returns a token set to designate the command as a target when
-creating an option.
+creating an option. The first command created is the default set which
+requires no sub command be called to initiate it; The standard flagset,
+for this set the first argument is the base header for the programs -h
+flag, for all consiquent calls to Command the first argument is the
+actual command token that is used on the command line to instigate the
+sub command.
 
-	cmd = conf.Command("doit", doitUsageString)
+	c := conf.Config{}
+	base = c.Command("MyHeader", MyUsageString)
+	cmd1 = c.Command("greet", MyUsageString)
+
+	app greet -s "Hello, World!"
 
 The cmd token is then used when defining an option, instructing the
-package that the option is to be assigned to the command. The option will
-appear in all of the commands for which tokens are provided, the tokes are
-separated by the | character, indicating that all the delineated tokens
-are to be used.
+package that the option is to be assigned to the command. The option
+will appear in all of the commands for which tokens are provided, the
+tokes are separated by the | character, indicating that all the
+delineated tokens are to be used.
 
-	conf.Option{
-		Commands: cmd | cmd1 | cmd2
+	c.Options{
+		{
+			Type:     conf.String,
+			Flag:     "s",
+			Default:  "Some string",
+			Usage:    stringUse,
+			Commands: base | cmd1 | cmd2,
+		},
 	}
 
-OPTIONS contain the data required to create a flag, which is done when the
-option is present within the active commands flagset, however options may
-also be modified by other methods, such as the user programme.
+OPTIONS contain the data required to create a flag, which is done when
+the option has been assigned to the active command.
 
 The `Check:` field takes a function value that may be defined whilst
 creating an option. This function has the signature `func(interface{})
 (interface{}, error)` which can be used to either specify tests and
-conditions for the options value or to change the value as it is passed.
+conditions upons the options value, or, to change the value as it is
+passed.
+
+	c.Options{
+		{
+			Type:     conf.String,
+			Flag:     "s",
+			Default:  "Some string",
+			Usage:    stringUse,
+			Commands: cmd | cmd1 | cmd2,
+			Check: func(interface{})(interface{}, error) {
+				str := *v.(*string)
+				if len(str) == 0 {
+					return v, fmt.Errorf("-s is empty)
+				}
+				return v, nil
+			},
+		},
+	}
 
 The following is an example of the conf package in use:
 
@@ -38,17 +70,18 @@ import (
 )
 
 var (
-	def = conf.Setup(helpBase, helpDef)
-	one = conf.Command("one", helpOne)
-	two = conf.Command("two", helpTwo)
+	c   = &conf.Config{}
+	def = c.Command(helpBase, helpDef)
+	one = c.Command("one", helpOne)
+	two = c.Command("two", helpTwo)
 )
 
 var opts = []conf.Option{
-	{Name: "intie",
+	{
 		Type:     conf.Int,
 		Flag:     "n",
 		Default:  12,
-		Usage:    intie,
+		Usage:    intUse,
 		Commands: def | one | two,
 		Check: func(v interface{}) (interface{}, error) {
 			i := *v.(*int)
@@ -58,11 +91,11 @@ var opts = []conf.Option{
 			return v, nil
 		},
 	},
-	{Name: "thing",
+	{
 		Type:     conf.String,
 		Flag:     "s",
-		Default:  "Some thing",
-		Usage:    thing,
+		Default:  "Some thing in the way she smiles",
+		Usage:    stringUse,
 		Commands: def | one | two,
 		Check: func(v interface{}) (interface{}, error) {
 			s := *v.(*string)
@@ -72,28 +105,28 @@ var opts = []conf.Option{
 			return v, nil
 		},
 	},
-	{Name: "none",
+	{
 		Type:     conf.Int,
 		Flag:     "i",
 		Default:  16,
 		Usage:    "the i is the none of all the ints",
 		Commands: def | one | two,
 	},
-	{Name: "verbosity",
+	{
 		Type:     conf.Int,
 		Flag:     "v",
 		Default:  0,
-		Usage:    "The overall chattiness of it all",
+		Usage:    "Oh! The overall chattiness of it all",
 		Commands: def | one | two,
 	},
 }
 
 func main() {
-	conf.Options(opts...)
-	if err := conf.Parse(); err != nil {
+	err := c.Compose(opts...)
+	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Printf("The current running mode is %q\n", conf.GetCmd())
+	fmt.Printf("The current running mode is %v\n", c.Running())
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -117,13 +150,13 @@ var helpDef = `
 MODES
         conf [mode] -[flag]
 
-	one     one does all things in the oneiest way.
+	one     one does all things that one should do.
 
-	two     two, despite appearances is second to none, doing things in an
-	        agreeable two like fashion.
+	two     two, despite appearances is second to none, achieving
+		things in no less than a second.
 
-	Further details of the use of each mode can be found by running the
-	following command.
+	Further details of the use of each mode can be found by running
+	the following command.
 
 	conf [mode] -help  or conf [mode] -h
 
@@ -131,11 +164,13 @@ FLAGS`
 
 var helpOne = `MODE
 	conf one
+		Best used like this ...
 
 FLAGS`
 
 var helpTwo = `MODE
 	conf two
+		Advisable to use like this ...
 
 FLAGS`
 
@@ -143,11 +178,12 @@ FLAGS`
 //  Flags
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-var intie = `This is the very default value in the most simple mode, to test
-if another way of writing the messages might be better.`
+var intUse = `This is the very default value in the most simplest of modes, to
+	test if another way of writing the messages might could possibly
+	be better.`
 
-var thing = `This is the default string thing, so as to best exemplify
-the use of this package in its current state I thought it
-best to write something very wordy here.`
+var stringUse = ` This is the default string dooing its thing, as to best
+	exemplify the use of this package in its current state; I
+	thought it best to write something particularly wordy here.`
 */
 package conf
